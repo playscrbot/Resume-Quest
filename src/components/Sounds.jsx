5091f1e160ea63d6eddfd6e70ec1832f3646576a
PlayScript <shivatheboss678@gmail.com>
2025-09-05 19:03:12 +0530

    Version 1.5

--- src/components/Sounds.jsx
+++ src/components/Sounds.jsx
@@ -1,10 +1,10 @@
 import { Howl } from 'howler';
 
 const SOUNDS = {
-  bgMusic: new Howl({ src: ["/public/sounds/bgm.wav"], volume: 0.3, loop: true }),
-  jump: new Howl({ src: ["/public/sounds/jump.wav"], volume: 0.3 }),
-  land: new Howl({ src: ["/public/sounds/land.wav"], volume: 0.3 }),
-  select: new Howl({ src: ["/public/sounds/select.wav"], volume: 0.5 })
+  bgMusic: new Howl({ src: ["/sounds/bgm.wav"], volume: 0.3, loop: true }),
+  jump: new Howl({ src: ["/sounds/jump.wav"], volume: 0.3 }),
+  land: new Howl({ src: ["/sounds/land.wav"], volume: 0.3 }),
+  select: new Howl({ src: ["/sounds/select.wav"], volume: 0.5 })
 };
 
 export default SOUNDS;
